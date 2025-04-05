24bb206ad97031fb535ac4d89a84b3854817de3b
d4nnoyer <49996258+d4nnoyer@users.noreply.github.com>
2025-04-05 22:56:03 +0700

    Add solution for LeetCode 1863. Sum of All Subset XOR Totals; Add tests;

---
+++ LeetCodeSolutions.Tests/Math/Easy/1863. Sum of All Subset XOR Totals Test.cs
@@ -0,0 +1,16 @@
+using LeetCodeSolutions.Solutions.Math.Easy;
+
+namespace LeetCodeSolutions.Tests.Math.Easy;
+
+public class Sum_of_All_Subset_XOR_Totals_Test {
+    
+    [TestCase(new[]{1, 3}, 6)]
+    [TestCase(new[]{5,1,6}, 28)]
+    [TestCase(new[]{3,4,5,6,7,8}, 480)]
+    public void Test(int[] nums, int expectedResult)
+    {
+        var indices = Sum_of_All_Subset_XOR_Totals.SubsetXORSum(nums);
+        
+        Assert.That(indices, Is.EqualTo(expectedResult));
+    }
+}
---
+++ LeetCodeSolutions/Solutions/Math/Easy/1863. Sum of All Subset XOR Totals.cs
@@ -0,0 +1,12 @@
+namespace LeetCodeSolutions.Solutions.Math.Easy;
+
+public class Sum_of_All_Subset_XOR_Totals {
+    public static int SubsetXORSum(int[] nums)
+    {
+        var result = 0;
+        foreach (var num in nums) 
+            result |= num;
+        
+        return result * (1 << nums.Length - 1);
+    }
+}
